77045687862ce3ddfed7f5115ebecbcf1e0133a5
5v37 <rus.bekerov@mail.ru>
2025-11-08 11:48:42 +0300

    Однократное получение параметра запуска

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,19 @@
+use std::sync::{Mutex, OnceLock};
+
+static FILE_PATH: OnceLock<Mutex<Option<String>>> = OnceLock::new();
+
 #[tauri::command]
 fn file_path() -> String {
-    if let Some(path) = std::env::args().nth(1) {
-        return path;
-    };
-    String::new()
+    if let Some(cell) = FILE_PATH.get() {
+        cell.lock().unwrap().take().unwrap_or_default()
+    } else {
+        String::new()
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    FILE_PATH.set(Mutex::new(std::env::args().nth(1))).unwrap();
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
